95bff398732e3e868cbe2d6e1918f332775c245a
mvl24 <mvl24@10.148.128.100>
2017-10-16 22:59:58 -0400

    Implemented DA Solution

--- DesignAssistant/src/geneticAlgorithm/DASolution.java
+++ DesignAssistant/src/geneticAlgorithm/DASolution.java
@@ -1,35 +1,71 @@
 package geneticAlgorithm;
 
-import org.uma.jmetal.solution.BinarySolution;
+import java.util.HashMap;
+
+
 import org.uma.jmetal.solution.Solution;
 import org.uma.jmetal.solution.impl.AbstractGenericSolution;
-import org.uma.jmetal.util.binarySet.BinarySet;
+
 
 import designAssistantCore.Configuration;
 
 public class DASolution extends AbstractGenericSolution<Configuration,DAProblem> {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8505580614299200682L;
 
-	protected DASolution(DAProblem problem) {
+	/** Constructor **/
+	public DASolution(DAProblem problem) {
 		super(problem);
+		initializeConfiguration();
+		initializeObjectiveValues();
 		// TODO Auto-generated constructor stub
+	}
+	
+	 /** Copy constructor */
+	public DASolution(DASolution solution) {
+		super(solution.problem);
+
+		for (int i = 0; i < problem.getNumberOfVariables(); i++) {
+			setVariableValue(i, new Configuration(solution.getVariableValue(i).getBinaryString()));
+		}
+
+		for (int i = 0; i < problem.getNumberOfObjectives(); i++) {
+			setObjective(i, solution.getObjective(i)) ;
+		}
+
+		attributes = new HashMap<Object, Object>(solution.attributes) ;
 	}
 
 	@Override
 	public String getVariableValueString(int index) {
 		// TODO Auto-generated method stub
-		return null;
+		return getVariableValue(0).toString();
 	}
 
 	@Override
 	public Solution<Configuration> copy() {
 		// TODO Auto-generated method stub
-		return null;
+		return new DASolution(this);
 	}
 
-	private void initializeConfiguration() {
-	    
-	      setVariableValue(0, new Configuration());
-	    
-	 }
+	private void initializeConfiguration() {	
+	    Configuration randomConfig = new Configuration(getRandomConfigurationString());
+		setVariableValue(0, randomConfig);
+	}
+	
+	private String getRandomConfigurationString(){
+		String config = Configuration.EMPTY;
+		int max_instruments = (int)(Math.random()*15)+1;
+		for (int j = 0; j < max_instruments; j++) {
+			int index = (int)(Math.random()*60);
+			if (index != 59)
+				config = config.substring(0, index) + "1" + config.substring(index+1);
+			else
+				config = config.substring(0, index) + "1";
+		}
+		return config;
+	}
 
 }
